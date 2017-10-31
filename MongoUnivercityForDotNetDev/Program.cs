6cc585942965780d920cddbae1412a2e5985f4e7
DIM-X-GOLDENSER\akhil <akhileshsh026@hotmail.com>
2017-11-01 00:16:46 +0530

    MongoClient

--- MongoUnivercityForDotNetDev/Program.cs
+++ MongoUnivercityForDotNetDev/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,21 @@ namespace MongoUnivercityForDotNetDev
     {
         static void Main(string[] args)
         {
+            MainAsync(args).GetAwaiter().GetResult();
+            Console.WriteLine();
+            Console.WriteLine("press enter");
+            Console.ReadLine();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+               var ConnectionString ="mongodb://localhost:27017";
+               var client = new MongoClient(ConnectionString);
+
+            // getting the databases schema commands with the help of clients.
+            var db =client.GetDatabase("test");
+            var col = db.GetCollection<Person>("movies");
+
         }
     }
 }
