aa11dd78d1aa5ce7bc3e3e84349d582f87a5f7a9
DIM-X-GOLDENSER\akhil <akhileshsh026@hotmail.com>
2017-11-01 21:59:54 +0530

    REplacement With the Id

--- MongoUnivercityForDotNetDev/Program.cs
+++ MongoUnivercityForDotNetDev/Program.cs
@@ -1,6 +1,7 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Bson.IO;
 using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Bson.Serialization.Conventions;
 using MongoDB.Driver;
 using System;
@@ -25,31 +26,29 @@ namespace MongoUnivercityForDotNetDev
         {
             var client = new MongoClient("mongodb://localhost:27017");
             var db = client.GetDatabase("test");
+            var col = db.GetCollection<BsonDocument>("widgets");
 
-            var col = db.GetCollection<Person>("People");
+            await db.DropCollectionAsync("widgets");
 
+            var docs = Enumerable.Range(0, 10).Select(i => new BsonDocument("_id", i).Add("x", i));
+            await col.InsertManyAsync(docs);
 
-            var builder = Builders<BsonDocument>.Filter;
+            var result = await col.ReplaceOneAsync(
+                new BsonDocument("_id", 5),
+                new BsonDocument("_id",5).Add("x",56)
+                );
+            await col.Find(new BsonDocument()).ForEachAsync(x => Console.WriteLine(x));
 
-            // var list = await col.Find(new BsonDocument()).Project("{Name:1,_id:0}").ToListAsync(); 
-            // or 
-            // var list = await col.Find(new BsonDocument()).Project(new BsonDocument("Name", 1).Add("_id", 0)).ToListAsync();
-            // or
-            // var list = await col.Find(new BsonDocument()).Project(Builders<BsonDocument>.Projection.Include("Name").Exclude("_id")).ToListAsync(); cahnge t0 BsonDocument in col.
-            // also you can do a Person specific Doc Projection as follows but also the BsonDocument above ,
-            // var list = await col.Find(new BsonDocument()).Project(Builders<Person>.Projection.Include("Name").Exclude("_id")).ToListAsync();
-            // so , to make this a person that only a person can use  
-            // var list = await col.Find(new BsonDocument()).Project<Person>(Builders<Person>.Projection.Include(x=>x.Name).Exclude(x=>x.Id)).ToListAsync();  //Expression lets make you Type Safe.
-            // and as the Person class is static so we didn't get the correct data for that class needs to static. 
-
-            var list = await col.Find(new BsonDocument())
-                        .Project(x => new { x.Name, calcAge = x.Age + 20 })
-                        .ToListAsync();
-            foreach (var item in list)
-            {
-                Console.WriteLine(item);
-            }
+            
         }
+
+        private class Widget
+        {
+            public int Id { get; set; }
+            [BsonElement("X")]  
+            public int x { get; set; }
+        }
+
 
         
     }
