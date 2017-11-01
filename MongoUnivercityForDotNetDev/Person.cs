e4462bc6129d7c1fb8daa4d739ee40358b01506e
DIM-X-GOLDENSER\akhil <akhileshsh026@hotmail.com>
2017-11-01 12:39:35 +0530

    Documents 2 , Person class addedd for Pocco respresnations

---
+++ MongoUnivercityForDotNetDev/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace MongoUnivercityForDotNetDev
+{
+    class Person
+    {
+        public ObjectId Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public List<string> Colors { get; set; }
+
+        public List<Pet> Pets { get; set; }
+
+        public BsonDocument ExtraElements { get; set; }
+    }
+
+    class Pet
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+    }
+}
